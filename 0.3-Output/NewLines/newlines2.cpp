fcd81f6b0fb704299bdbb1685ff8fa8eb6b0fb60
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-11-17 17:53:27 -0300

    GetStarted, Syntax e Output

---
+++ 0.3-Output/NewLines/newlines2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << "\n"; // é possivel escrever o \n depois, como outra string
+    cout << "I am learning C++";
+    return 0;
+}
---
+++ 0.3-Output/NewLines/newlines3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world!" << "\n\n"; // dois /n juntos pularam uma linha; além do /n podemos usar endl para pular linha 
+    cout << "I am learning C++";
+    return 0;
+}
